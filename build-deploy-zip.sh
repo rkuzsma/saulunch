9a666b405b26663ef6cc36777447d62994f5adfb
Rich Kuzsma <rkuzsma@gmail.com>
2016-09-03 08:41:35 -0400

    Update September calendar

--- build-deploy-zip.sh
+++ build-deploy-zip.sh
@@ -5,14 +5,15 @@
 this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 output_dir=$this_script_dir/build
 rm -rf $output_dir
+mkdir $output_dir
 mkdir $output_dir/bin
 mkdir $output_dir/lib
-mkdir $output_dir/resources
+mkdir -p $output_dir/resources/generated
 cp -r $this_script_dir/bin $output_dir
 cp -r $this_script_dir/lib $output_dir
-cp -r $this_script_dir/resources $output_dir
+cp -r $this_script_dir/resources/generated $output_dir/resources
 cp $this_script_dir/package.json $output_dir/package.json
-cp $this_script_dir/index.js $output_dir/index.json
+cp $this_script_dir/index.js $output_dir/index.js
 cd $output_dir
 npm install --production
 # Remove 108MB of unnecessary test data from pdf2json
